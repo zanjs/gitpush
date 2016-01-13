04799f5a1799941492149f2df017e43804c65f6f
zanjs <root@zanjs.com>
2016-01-13 09:36:39 +0800

    Wed Jan 13 2016 09:36:39 GMT+0800 (中国标准时间) By gitpush

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 var chalk = require('chalk');
 var test = require('child_process').exec;
 
-var emoji = require('./js/emoji');
+var emoji = require('./js/emoji').emoji;
 
 
-console.log(emoji.emoji.length);
+console.log(emoji.length);
 
 var check = test('git add -u -n', function(err, stdout, stderr){
   if(stdout.length == 0)
