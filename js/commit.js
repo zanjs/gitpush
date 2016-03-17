f22247d2b89e285dd8ceeb9cb8dbafe01b247810
zanjs <root@zanjs.com>
2016-03-17 09:26:28 +0800

    :sweat_smile: - git  from gitpush

--- js/commit.js
+++ js/commit.js
@@ -1,7 +1,12 @@
 var commit = [
     '今天是个好日子',
     '今天好懒',
-    '不想写代码'
+    '不想写代码',
+    '回滚版本到分支中的某一个commit时间点 git reset [commit 哈希值]',
+    'git log 查看 commit日志',
+    '创建一个叫做"dev"的分支，并切换过去',
+    '切换回主分支 git checkout master',
+    '再把新建的分支删掉： git branch -d dev'
 ]
 
  exports.commit = commit;
