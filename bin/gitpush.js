337078ac3b52a51bcc8ca3d33da1de84cf48e9da
zanjs <root@zanjs.com>
2016-04-02 15:32:58 +0800

    不想写代码:racehorse:

---
+++ bin/gitpush.js
@@ -0,0 +1,159 @@
+#!/usr/bin/env node
+
+'use strict';
+
+var chalk = require('chalk');
+var test = require('child_process').exec;
+
+var emoji = require('../js/emoji').emoji,
+    commit = require('../js/commit').commit,
+    emojiLenth = emoji.length,
+    randomNum = GetRandomNum(0,emojiLenth),
+    commitUI = commit[GetRandomNum(0,commit.length)],
+    emojiUI = emoji[randomNum];
+
+
+var spawn = require('child_process').spawn;
+
+var check = test('git add -u -n', function(err, stdout, stderr){
+  if(stdout.length == 0)
+    console.log(chalk.red.bold('No Files Modified.'));
+
+    argvtest();
+});
+
+function GetRandomNum(Min,Max){
+  var Range = Max - Min;
+  var Rand = Math.random();
+  return(Min + Math.round(Rand * Range));
+}
+
+
+function argvtest() {
+  var commitMsg = process.argv[2];
+  if(commitMsg){
+         commitMsg = commitMsg.trim();      
+  }
+  
+//   console.log(commitMsg);
+
+// var fstr = commitMsg.substr(0,1);
+//                      if(fstr != "#"){
+//                          commitMsg = commitMsg + emojiUI
+//                      } 
+//                      console.log(fstr);
+//                      console.log(commitMsg);
+
+//   return false;
+  if(!commitMsg)
+    commitMsg = commitUI;
+ autogit(commitMsg);
+}
+
+function autogit(commitMsg) {
+  
+  var st,sTime;
+
+  sTime = new Date().getTime();
+  console.log(new Date()+'\nstart || 开始执行...');
+
+  var status = spawn('git', ['status', '-s', '-uno']);
+
+  status.stdout.on('data', function(data){
+    st = data.toString().split('\n');
+    for(var i=0;i<st.length;i++) {
+      console.log(chalk.red(st[i].trim()));
+    }
+  });
+
+  status.on('close', function(){
+    //var add = spawn('git', ['add', '-u', '-v']);
+    var add = spawn('git', ['add', '.']);
+    add.stdout.on('data', function(data){
+      console.log(chalk.blue(data.toString()));
+    });
+
+    add.on('close', function(){
+        
+                     var fstr = commitMsg.substr(0,1);
+                     if(fstr != "#"){
+                         commitMsg = commitMsg + emojiUI
+                     } 
+                                 
+                    var commit = spawn('git', ['commit', '-m', commitMsg]);
+                    commit.on('close', function(){
+                            console.log(chalk.green.bold('git commit ok \n 正在提交到远程仓库 \n loading push...'));
+
+                            var push = spawn('git', ['push']);
+
+                            push.stdout.on('data', function(data){
+                            console.log(chalk.blue(data.toString()));
+                            });
+
+                            var isok = true;
+
+                            push.stderr.on('data', function(data){
+                                var stc = data.toString();
+                                console.log('[stderr]♣♣:'+chalk.blue(stc));
+                                if(stc == 'Everything up-to-date'){
+                                    console.log('isok ='+ false);
+                                    isok = false;
+                                }
+
+                            });
+
+
+                            push.on('close', function(){
+                            //console.timeEnd("push-time");
+                            if(isok == false) return;
+
+                            var eTime = new Date().getTime(),
+                                useTime = eTime - sTime;
+                            console.log(chalk.green.bold('git push ok \n time cost: '+useTime +'ms\n恭喜您：推送完成 || 耗时 '+ useTime/1000 +'秒\n更多信息请看上面stderr:信息提示'));
+                            });
+
+                    });
+    });
+  });
+}
+
+
+function autoInstall(call) {
+    
+    var install = spawn('npm', ['install', 'gitpush','-g']);
+    
+    
+    install.stdout.on('data', function(data){
+          console.log(chalk.blue(data.toString()));
+    });
+    
+    
+    install.stderr.on('data', function(data){
+        var stc = data.toString();       
+            console.log(stc);            
+     });
+    
+     install.on('close', function(){
+         
+         call();
+         
+     })
+    
+}
+
+
+function install() {    
+    
+     var status = spawn('npm', ['install', 'gitpuh']);
+
+    status.stdout.on('data', function(data){
+    
+    });
+    
+    status.on('close', function(){
+         
+        console.log("完成")
+         
+     })
+    
+}
