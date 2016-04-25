414f11684992965f9d0fec1da56b8c046afdbec0
zanjs <root@zanjs.com>
2016-04-25 09:36:49 +0800

    add commits English Inspirational quotes famous  :feelsgood:

--- js/commit.js
+++ js/commit.js
@@ -6,6 +6,15 @@ var commit = [
     'git log 查看 commit日志',
     '创建一个叫做"dev"的分支，并切换过去',
     '切换回主分支 git checkout master',
+    'True mastery of any skill takes a lifetime.',
+    'Sweat is the lubricant of success.',
+    'If you are doing your best,you will not have to worry about failure.',
+    'Energy and persistence conquer all things.',
+    'Bravery never goes out of fashion.',
+    'Those who turn back never reach the summit.',
+    'Proper preparation solves 80 percent of life\'s problems',
+    'Winners do what losers don\'t want to do',
+    'Every noble work is at first impossible',
     '再把新建的分支删掉： git branch -d dev'
 ]
 
